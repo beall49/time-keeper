b85b05529f4297a296e198ff4c2175e69536edc0
beall49 <beall49@gmail.com>
2017-02-22 09:44:57 -0800

    updating cert

--- routes/cert.js
+++ routes/cert.js
@@ -3,7 +3,7 @@ let router = express.Router();
 
 /* GET cert  page. */
 router.get('/JPXndW3Pb-LIPm6owHg_aoWPBXRU0W0mAW94TS-nDwg', function(req, res, next) {
-    res.send('JJPXndW3Pb-LIPm6owHg_aoWPBXRU0W0mAW94TS-nDwg.HThcSBDIO_752vpqj-b0Dx6CztXVwI-lcy5hwLwbwwA')
+    res.send('JPXndW3Pb-LIPm6owHg_aoWPBXRU0W0mAW94TS-nDwg.HThcSBDIO_752vpqj-b0Dx6CztXVwI-lcy5hwLwbwwA')
 });
 
 
